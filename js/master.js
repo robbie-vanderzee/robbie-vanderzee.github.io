b941d4b38ad7d8a67d28d183f3eb36ee74c886a8
Robbie VanDerzee <Robert.Scott.VanDerzee@gmail.com>
2020-05-22 02:37:45 -0400

    \bad js scramble + sidebar nav active

--- js/master.js
+++ js/master.js
@@ -1,37 +1,37 @@
 class TextScramble {
-  static chars = 'qwertyuiopasdfghjklzxcvbn'
+  static chars = 'qwertyuiopasdfghjklzxcvbn';
   constructor(el) {
-    this.el = el
-    this.update = this.update.bind(this)
+    this.el = el;
+    this.update = this.update.bind(this);
   }
+
   setText(newText) {
-    const oldText = this.el.innerText
-    const length = Math.max(oldText.length, newText.length)
-    const promise = new Promise((resolve) => this.resolve = resolve)
-    this.queue = []
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => this.resolve = resolve);
+    this.queue = [];
     for (let i = 0; i < length; i++) {
-      const from = oldText[i] || ''
-      const to = newText[i] || ''
-      const start = i
-      // Theory: golden ratio randomness between golden logarithmic growth function
-      const center = Math.floor(38.2 * Math.log(start + 2))
-      const margin = Math.floor(38.2 * Math.log(start + 3)) - center
-      const end = Math.floor(center) + Math.floor(margin * Math.random())
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = i;
+      const center = Math.floor(38.2 * Math.log(start + 2));
+      const margin = Math.floor(38.2 * Math.log(start + 3)) - center;
+      const end = Math.floor(center) + Math.floor(margin * Math.random());
       this.queue.push({
         from,
         to,
         start,
         end
-      })
+      });
     }
-    cancelAnimationFrame(this.frameRequest)
-    this.frame = 0
-    this.update()
-    return promise
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
   }
   update() {
-    let output = ''
-    let complete = 0
+    let output = '';
+    let complete = 0;
     for (let i = 0, n = this.queue.length; i < n; i++) {
       let {
         from,
@@ -39,58 +39,88 @@ class TextScramble {
         start,
         end,
         char
-      } = this.queue[i]
+      } = this.queue[i];
       if (this.frame >= end) {
-        complete++
-        output += to
+        complete++;
+        output += to;
       } else if (this.frame >= start) {
         if (!char || Math.random() < 0.146) {
-          char = this.randomChar()
-          this.queue[i].char = char
+          char = this.randomChar();
+          this.queue[i].char = char;
         }
-        output += `${char}`
+        output += `${char}`;
       } else {
-        output += from
+        output += from;
       }
     }
-    this.el.innerHTML = output
+    this.el.innerHTML = output;
     if (complete === this.queue.length) {
-      this.resolve()
+      this.resolve();
     } else {
-      this.frameRequest = requestAnimationFrame(this.update)
-      this.frame++
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
     }
   }
   randomChar() {
-    return TextScramble.chars[Math.floor(Math.random() * TextScramble.chars.length)]
+    return TextScramble.chars[Math.floor(Math.random() * TextScramble.chars.length)];
   }
 }
 
-/*
-var mainNavLinks = document.getElementsByClassName("button nav")
-var mainSections = document.getElementsByClassName("body-section")
-console.log(mainNavLinks)
-console.log(mainSections)
-https://css-tricks.com/sticky-smooth-active-nav/
-*/
-window.onload = function() {
 
-  var scrambleparents = document.getElementsByClassName("scramble-container")
+window.onload = function() {
+  let scrambleparents = document.getElementsByClassName("scramble-container");
+  let scramblers;
   for (let parent of scrambleparents) {
-    f(parent)
+    f(parent);
   }
 
   function f(x) {
-    var scrambles = x.getElementsByClassName("scramble")
-    var scramblers = new Map()
+    var scrambles = x.getElementsByClassName("scramble");
+    var scramblers = new Map();
     for (let scrambler of scrambles) {
-      scramblers.set(new TextScramble(scrambler), scrambler.textContent)
+      scramblers.set(new TextScramble(scrambler), scrambler.textContent);
     }
 
     x.onmouseenter = function() {
       for (let [scrambler, content] of scramblers) {
-        scrambler.setText(content)
+        scrambler.setText(content);
       }
     }
+  }
+
+  let sections = document.getElementsByTagName('section');
+  let section_nav_buttons = document.getElementsByClassName('button nav');
+  createObserver();
+
+  function createObserver(){
+  	let observer;
+  	let options = {
+  		root: null,
+  		rootMargin: "-50% 0% -50% 0%"
+  	}
+  	observer = new IntersectionObserver(handleIntersect, options);
+  	for(let section of sections){
+  		observer.observe(section);
+  	}
+  }
+
+  function handleIntersect(entries, observer) {
+  	entries.forEach((entry) => {
+      var index = entry.target.getAttribute('num');
+      if(entry.isIntersecting) {
+        var x = section_nav_buttons[index];
+        x.classList.add('active');
+        var scrambles = x.getElementsByClassName("scramble");
+        var scramblers = new Map();
+        for (let scrambler of scrambles) {
+          scramblers.set(new TextScramble(scrambler), scrambler.textContent);
+        }
+        for (let [scrambler, content] of scramblers) {
+            scrambler.setText(content);
+        }
+      } else {
+        section_nav_buttons[index].classList.remove('active');
+      }
+  	});
   }
 }
